36b4fbb4e344341a42d2423cf09ffa540b72cd7e
OthmaneSeon <othmane@seon.group>
2022-12-18 15:53:57 +0100

    add a scope to get current month lunches

--- app/models/mystery_lunch.rb
+++ app/models/mystery_lunch.rb
@@ -2,4 +2,5 @@ class MysteryLunch < ApplicationRecord
   has_many :mystery_lunch_employee_relation
   has_many :employees, through: :mystery_lunch_employee_relation
 
+  scope :current_month, -> { where(created_at: Date.current.beginning_of_month) }
 end
