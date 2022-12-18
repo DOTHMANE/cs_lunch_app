b59e02bff12c2e758a9c3fcc63b837da56c97269
OthmaneSeon <othmane@seon.group>
2022-12-18 15:54:43 +0100

    add a method to check if a lunch is full

--- app/models/mystery_lunch.rb
+++ app/models/mystery_lunch.rb
@@ -3,4 +3,9 @@ class MysteryLunch < ApplicationRecord
   has_many :employees, through: :mystery_lunch_employee_relation
 
   scope :current_month, -> { where(created_at: Date.current.beginning_of_month) }
+
+  def full?
+    employees.count == 3
+  end
+
 end
