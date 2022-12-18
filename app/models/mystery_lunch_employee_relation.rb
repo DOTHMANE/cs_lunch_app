b67c379aaf0b663fe279cd1e8060221f047440fc
OthmaneSeon <othmane@seon.group>
2022-12-18 15:45:18 +0100

    add a class method to check if 2 employees had lunch in the last 3 months

--- app/models/mystery_lunch_employee_relation.rb
+++ app/models/mystery_lunch_employee_relation.rb
@@ -3,4 +3,14 @@ class MysteryLunchEmployeeRelation < ApplicationRecord
   belongs_to :employee
 
   scope :last_three_months, ->(date_max, date_min) { where('created_at <= ? AND created_at >= ?', date_max, date_min) }
+
+  def self.had_lunch_last_three_months?(employee_a, employee_b)
+    lunch_employees = last_three_months(Date.current.next_month, Date.current.months_ago(3)).pluck(:mystery_lunch_id,
+                                                                                                   :employee_id)
+                                                                                            .group_by(&:shift)
+                                                                                            .transform_values(&:flatten)
+
+    lunch_employees.values.include? [employee_a, employee_b] or lunch_employees.values.include? [employee_b, employee_a]
+  end
+
 end
