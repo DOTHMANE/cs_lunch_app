6032d059ae35e1975fd5413fa68200bae9cf35ad
OthmaneSeon <othmane@seon.group>
2022-12-18 15:44:14 +0100

    add scope to get last three months lunch employees

--- app/models/mystery_lunch_employee_relation.rb
+++ app/models/mystery_lunch_employee_relation.rb
@@ -1,4 +1,6 @@
 class MysteryLunchEmployeeRelation < ApplicationRecord
   belongs_to :mystery_lunch
   belongs_to :employee
+
+  scope :last_three_months, ->(date_max, date_min) { where('created_at <= ? AND created_at >= ?', date_max, date_min) }
 end
