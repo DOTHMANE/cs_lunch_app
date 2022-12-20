e859ee2fbbd214bc74f0b3078cd668f3cda47ef4
OthmaneSeon <othmane@seon.group>
2022-12-20 10:00:09 +0100

    add specs

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Employee, type: :model do
-  let(:department) { create(:department, name: 'employee department') }
-  let(:employee) { create(:employee, department: department) }
-  let(:department_b) { create(:department, name: 'employee department b') }
-  let(:employee_b) { create(:employee, department: department_b) }
-  let(:mystery_lunch) { create(:mystery_lunch) }
+  let!(:department) { create(:department, name: 'employee department') }
+  let!(:employee) { create(:employee, department: department) }
+  let!(:department_b) { create(:department, name: 'employee department b') }
+  let!(:employee_b) { create(:employee, department: department_b) }
+  let!(:mystery_lunch) { create(:mystery_lunch) }
   let!(:lunch_employee_a) { create(:mystery_lunch_employee_relation, employee: employee, mystery_lunch: mystery_lunch) }
   let!(:lunch_employee_b) { create(:mystery_lunch_employee_relation, employee: employee_b, mystery_lunch: mystery_lunch) }
 
@@ -45,8 +45,16 @@ RSpec.describe Employee, type: :model do
   describe '.add_default_cover' do
     it 'sets a default photo to employees' do
       employee.send(:add_default_cover)
-      binding.pry
+
       expect(employee.reload.photo.attached?).to eq(true)
+    end
+  end
+
+  describe '.assign_new_employee' do
+    it 'assigns new employee to a mystery lunch' do
+      new_employee = create(:employee, department: create(:department))
+
+      expect(new_employee.reload.mystery_lunches.count).to eq(1)
     end
   end
 end
