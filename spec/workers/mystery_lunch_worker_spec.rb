523772e243286e2709e1ec850a570dde709f4728
OthmaneSeon <othmane@seon.group>
2022-12-20 11:05:07 +0100

    add sidekiq-scheduler spec

---
+++ spec/workers/mystery_lunch_worker_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+require "fugit"
+
+RSpec.describe "sidekiq-scheduler" do
+  sidekiq_file = File.join(Rails.root, "config", "sidekiq_scheduler.yml")
+  schedule = YAML.load_file(sidekiq_file)['default']['mystery_lunch_worker']
+
+  describe "cron syntax" do
+      it "#{schedule["cron"]} has correct cron syntax" do
+        expect { Fugit.do_parse(schedule["cron"]) }.not_to raise_error
+      end
+  end
+
+  describe "job classes" do
+      it "#{schedule["class"]} class in /jobs" do
+        expect { schedule["class"].constantize }.not_to raise_error
+      end
+  end
+end
