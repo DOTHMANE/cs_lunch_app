7f4077bf5a7a6df36b4cf81d5b394757f7c84c27
OthmaneSeon <othmane@seon.group>
2022-12-20 09:22:39 +0100

    Add a callback to add default photo to employee after creation

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -7,5 +7,15 @@ class Employee < ApplicationRecord
   validates :department, presence: true
   validates :name, presence: true
 
+  after_commit :add_default_cover, on: [:create]
+
   scope :active, -> { where(status: 1) }
+
+  private
+
+  def add_default_cover
+    unless photo.attached?
+      self.photo.attach(io: File.open(Rails.root.join("app", "assets", "images", "user.png")), filename: 'user.png' , content_type: "image/png")
+    end
+  end
 end
